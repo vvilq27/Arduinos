cb5dfcdf32119a5145b589bd7382a44496ecddc3
vvilq27 <vvilq27@gmail.com>
2018-04-18 21:12:09 +0200

    40khz sample and display, voice quite bad, small dissortions

--- myNRFrcv/myNRFrcv.ino
+++ myNRFrcv/myNRFrcv.ino
@@ -2,7 +2,8 @@
 #include <RF24.h>
 #include <SPI.h> 
 
-#define BUFFER_SIZE 28
+#define BUFFER_SIZE 60
+#define LED 3
 
 RF24 myRadio(7,8);
 byte addresses[][6] = {"253"};
@@ -16,18 +17,19 @@ Audio audioPack;
 byte volatile audioIndex;
 byte volatile displayDone = 1;
 int volatile rcvPacketCnt = 0;
+volatile byte vol = 2;
 
-/*
+/*==========================
  *        FUNCTIONS
- */
+============================*/
 
 void timer1FastPwmAInit(void){
   TCCR1A = _BV(COM1A1) | _BV(WGM11)| _BV(COM1B0) | _BV(COM1B1);
   TCCR1B = _BV(WGM13) | _BV(WGM12) | _BV(CS10);
   TIMSK1 |= _BV(TOIE1);
-  ICR1 = 888;
+  ICR1 = 390;
   DDRB |= _BV(PB1)| _BV(PB2);   // pwm pin as output  D9
-  OCR1B = 455;
+  OCR1B = 123;
 }
 
 /*
@@ -43,14 +45,39 @@ void adcAudio(void){
   ADCSRA |= (1 << ADATE); //enabble auto trigger
   ADCSRA |= (1 << ADEN); //enable ADC
   ADCSRA |= (1 << ADSC); //start ADC measurements
-   ADCSRA |= (1 << ADIE);
+  ADCSRA |= (1 << ADIE);
 }
 */
 
+void T2_init(void){
+  TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20); //1024 presc
+//  TIMSK2 |= (1 << TOIE2);
+}
+
+void adc_init(int channels){
+  ADMUX = 0;
+  ADMUX |= ( 1<<REFS0 ) | (1<<ADLAR) | channels;//Vref - Vcc, left adjust result
+  ADCSRA = 0;
+  ADCSRA |= ( 1<<ADEN )|( 1<<ADPS2 )|( 1<<ADPS0 ); //ADC on, 32 prescaler
+}
+
+inline void pomiar(uint8_t channel){
+  ADMUX = (ADMUX & 0xF8)|channel;
+  ADCSRA |= (1<<ADSC);// measure start
+  while( ADCSRA & (1<<ADSC) ); // wait for meas to complete
+}
+
+/*============================
+ *          SETUP
+ =============================*/
 void setup() {
   timer1FastPwmAInit();
 
+  adc_init(3);
+  T2_init();  //int disabled
+  
   Serial.begin(115200);
+  Serial.println(ICR1);
   
   myRadio.begin();  
   myRadio.setChannel(115); 
@@ -60,23 +87,36 @@ void setup() {
   myRadio.startListening();
 
   sei();
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+  delay(900);
+  digitalWrite(LED, LOW);
 }//end setup
 
 /*============================
  *        MAIN LOOP
  =============================*/
+
+ //add other buffer, maybe it helpsMM
 void loop() {
-  
-  if ( myRadio.available() ) { //&& displayDone
+  /*
+  //signal that frames come faster than display ends
+  if(myRadio.available() && !displayDone){
+    digitalWrite(LED, HIGH);
+  }
+  */
+  if ( myRadio.available() && displayDone){
     while (myRadio.available()){
        myRadio.read(&audioPack, sizeof(audioPack));
     }
-
+    displayDone = 0;
+/*
     rcvPacketCnt++;
-//    displayDone = 0;
-Serial.print(audioPack.id);
-Serial.print(" ");
+    
+    Serial.print(audioPack.id);
+    Serial.print(" ");
     Serial.println(audioPack.id-rcvPacketCnt);
+*/    
 /*
     for(int i =0; i <5; i++){
       for(int j =0; j <5; j++){
@@ -86,7 +126,8 @@ Serial.print(" ");
       Serial.println();
     }
  */
-  }
+    TCCR1A |= _BV(COM1A1)| _BV(COM1B0) | _BV(COM1B1);//turn on speaker
+  }//end if
   
   
   /* works on .write(
@@ -100,10 +141,33 @@ if ( myRadio.available() ) { //&& displayDone
 
 }//end main loop
 
+/*=========================
+ *       INTERUPTS
+=========================== */
+
+
+ISR(TIMER2_OVF_vect){
+   pomiar(0);
+   ICR1 = ADCH * 256;
+   pomiar(1);
+   vol = ADCH / 25;
+   Serial.print(ICR1);
+   Serial.print("  ");
+   Serial.println(OCR1A);
+   
+}
+
+//przezucic wpis do rejestrow do innego ISR?
+//pomierzyc czasy wykonania przerwan timerem
+//ustawic timer do obslugi jakis funkcjonalnosci(reset diody)
+//stworzyc buffer na 1kb ram i tam ladowac przychodzace dane
 ISR(TIMER1_OVF_vect){
-  OCR1A = OCR1B = audioPack.audioBuf[audioIndex++];
+  if(!displayDone)
+    OCR1A = OCR1B = audioPack.audioBuf[audioIndex++] * vol;
+
   if(audioIndex > BUFFER_SIZE){
     audioIndex = 0;
     displayDone = 1;
+    TCCR1A &= ~(_BV(COM1A1) | _BV(COM1B0) | _BV(COM1B1)); //turn off speaker
   }
 }
--- myNRFtrns/myNRFtrns.ino
+++ myNRFtrns/myNRFtrns.ino
@@ -2,7 +2,8 @@
 #include <RF24.h>
 #include <SPI.h>
 
-#define BUFFER_SIZE 28
+#define BUFFER_SIZE 60
+#define LED_PIN 6
 
 RF24 myRadio(7,8);
 byte addresses[][6] = {"253"};
@@ -27,19 +28,20 @@ void adcAudio(void){
   ADMUX |= (1 << REFS0); //set reference voltage
   ADMUX |= (1 << ADLAR); //left align the ADC value- so we can read highest 8 bits from ADCH register only
   
-  ADCSRA |= (1 << ADPS2) | (1 << ADPS1)| (1 << ADPS0); //set ADC clock with 128 prescaler- 16mHz/128 9khz
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS0); //set ADC clock with 32 prescaler- 16mHz/32 38khz
   ADCSRA |= (1 << ADATE); //enabble auto trigger
   ADCSRA |= (1 << ADEN); //enable ADC
   ADCSRA |= (1 << ADSC); //start ADC measurements
   ADCSRA |= (1 << ADIE);
 }
 
+
 /*=========================
  *        SETUP
 =========================== */
 void setup() {
   pinMode(3, INPUT_PULLUP);
-  
+
   Serial.begin(115200);
   myRadio.begin();  
   myRadio.setChannel(115); 
@@ -48,14 +50,16 @@ void setup() {
   myRadio.openWritingPipe( addresses[0]);
   
   adcAudio();
-
-attachInterrupt(digitalPinToInterrupt(3), reset, FALLING);
+  pinMode(LED_PIN, OUTPUT);
+//attachInterrupt(digitalPinToInterrupt(3), reset, FALLING);
 }//end of setup
 
+/*
 //enabling next send
 void reset(){
   resetPrint = 1;
 }
+*/
 
 
 /*=========================
@@ -63,44 +67,54 @@ void reset(){
 =========================== */
 void loop() {
   // put your main code here, to run repeatedly:
+  if(!digitalRead(3)){
+    //very jiggy
+    ADCSRA |= (1 << ADIE);
+    digitalWrite(LED_PIN, HIGH);
+  }
+  
 }
 
 /*=========================
  *       INTERUPTS
 =========================== */
+
+
 //getting data and sending it
 ISR(ADC_vect){
+  
   //fill buffer before sending it
   if(audioIndex<BUFFER_SIZE)
     audioPack.audioBuf[audioIndex++] = ADCH;
   //if buffer full
   else{
     ADCSRA &= ~(1 << ADIE);
-    
-    if(resetPrint){
-      if(myRadio.write(&audioPack, sizeof(audioPack)))
-        Serial.println("packet sent");
-      else{
-        Serial.println("send error");
-        ADCSRA |= (1 << ADIE);
-        resetPrint = 0;
-        audioIndex = 0;
-        return;
+    myRadio.writeFast(&audioPack, sizeof(audioPack));
+/*
+    if(myRadio.write(&audioPack, sizeof(audioPack)))
+      Serial.println("packet sent");
+    else
+      Serial.println("send error");
+
+    for(int i =0; i <5; i++){
+      for(int j =0; j <5; j++){
+        Serial.print(audioPack.audioBuf[i*5 + j]);
+        Serial.print(" ");
       }
-      
-      for(int i =0; i <5; i++){
-        for(int j =0; j <5; j++){
-          Serial.print(audioPack.audioBuf[i*5 + j]);
-          Serial.print(" ");
-        }
-        Serial.println();
-      }
-      
-      Serial.println(audioPack.id++);
-      resetPrint = 0;
-      audioIndex = 0;
+      Serial.println();
     }
-    ADCSRA |= (1 << ADIE);
+    
+    Serial.println(audioPack.id++);
+    resetPrint = 0;
+ */
+    audioIndex = 0;
+    audioPack.id++;
+//    ADCSRA |= (1 << ADIE);
   }
+  digitalWrite(LED_PIN, LOW);
 }
+
+//======== questions ===========
+// how long does it take to send packet and it is possible to send it when new packet is formed
+//what if i sent bigger packets than 32B
 
